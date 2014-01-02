1512f447d6b9d5e583144e1997649c3702f4a62d
Robin Winslow <robin@robinwinslow.co.uk>
2014-01-02 01:53:18 +0000

    Github and StackOverflow APIs

--- cv/views.py
+++ cv/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from urllib.request import urlopen
+from io import BytesIO
+import json
+import gzip
 
-# Create your views here.
+def homepage(request):
+    githubUser = jsonResponse('https://api.github.com/users/nottrobin')
+    stackUser  = jsonResponse('http://api.stackoverflow.com/1.1/users/613540/')['users'][0]
+
+    return render(
+        request,
+        'cv/index.html',
+        {
+            'title'       : 'Web developer',
+            'repositories': githubUser['public_repos'],
+            'reputation'  : stackUser['reputation']
+        }
+    )
+
+def jsonResponse(url):
+    response = urlopen(url)
+
+    if response.info().get('Content-Encoding') == 'gzip':
+        binaryStream = BytesIO(response.readall()  )
+        zipFile = gzip.GzipFile(fileobj=binaryStream)
+        responseContents = zipFile.read().decode('utf-8')
+    else:
+        responseContents = response.readall().decode('utf-8')
+
+    return json.loads(responseContents)
--- cvsite/urls.py
+++ cvsite/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from cv import views
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="cv/index.html")),
+    url(r'^$', views.homepage, name='vote'),
     url(r'^admin/', include(admin.site.urls)),
 )
